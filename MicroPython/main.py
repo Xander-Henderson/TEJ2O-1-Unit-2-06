5407e892d847943aba350ccc4a2bc93877ea087b
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-09-26 16:34:49 +0000

    turns on and off a LED

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,24 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Alexander
+Created on: Sep 2025
+This module turns on and off a LED.
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+
+while True:
+   if button_a.is_pressed():
+       # turns on pin 16
+       display.show(Image.YES)
+       pins.digital_write_pin(DigitalPin.P16, 1)
+
+
+while True:
+   if button_b.is_pressed():
+       # turns off pin 16
+       display.show(Image.NO)
+       pins.digital_write_pin(DigitalPin.P16, 0)
